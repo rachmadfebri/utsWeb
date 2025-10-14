0598114c08076ee0ffbd6f72b76c578c8f8bb1ec
Rachmad Febriananda <rachmad17845@gmail.com>
2025-10-14 11:45:31 +0700

    add style & responsive for top bar & profile

---
+++ profile.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  {
+    threshold: 0.1,
+  }
+);
+
+const elementsToAnimate = document.querySelectorAll(".reveal-on-scroll");
+elementsToAnimate.forEach((el) => observer.observe(el));
